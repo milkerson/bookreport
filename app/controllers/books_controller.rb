46131fd90b074cb6a66ad14b59313bfd31da5b8d
Mike Wilkerson <wilkerson@mac.com>
2016-02-11 10:41:14 -0600

    Added carrierwave gem and file uploads

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :series, :num_pages, :isbn, :genre)
+      params.require(:book).permit(:title, :author, :series, :num_pages, :isbn, :genre, :cover)
     end
 end
